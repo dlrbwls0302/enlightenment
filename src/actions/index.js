4cd04753d353b7898933e2cc7e1d852cdf9c3c0e
Kyu Jin Lee <dcohornt@gmail.com>
2021-04-05 15:06:29 +0900

    [feat/news/completed][css만 추가하면 됨]

---
+++ src/actions/index.js
@@ -0,0 +1,30 @@
+export const CHANGE_LOADING = 'CHANGE_LOADING';
+export const SET_ARTICLES = 'SET_ARTICLES';
+export const CHANGE_QUERY = 'CHANGE_QUERY';
+
+export const changeLoading = () => {
+    return{
+        type: CHANGE_LOADING,
+        payload:{
+
+        }
+    }
+}
+
+export const setArticles = (articles) => {
+    return{
+        type: SET_ARTICLES,
+        payload:{
+            articles
+        }
+    }
+}
+
+export const changeQuery = (e) => {
+    return {
+        type: CHANGE_QUERY,
+        payload:{
+            query: e.target.value
+        }
+    }
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from 'redux';
-
+import newsReducer from './newsReducer';
 
 
 const rootReducer = combineReducers({
-    
+    newsReducer,
 })
 
 export default rootReducer;
