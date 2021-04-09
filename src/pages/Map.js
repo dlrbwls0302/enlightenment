73c6265c642f288e0533665ee2a75172d8b9e029
juyeop <shininggiver@gmail.com>
2021-04-09 13:07:09 +0900

    [feat/mapPage/working][지도에 마커 찍기 기능 구현 완료]

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,16 +1,17 @@
-/*global kakao*/ 
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
 import { useSelector } from 'react-redux'
 import '../styles/Map.css';
 import KakaoMap from '../components/KakaoMap'
+// import { createPortal } from 'react-dom';
 
 const Map = () => {
     const state = useSelector((state) => {
         return state.electionsReducer
     })
     const [markerPositions, setMarkerPositions] = useState([])
-    const [places, setPlaces] = useState([]);
+    ////
+    // const [places, setPlaces] = useState([]);
     const [sgId, setSgId] = useState('');
     const [sdName, setSdName] = useState('');
     const [wiwName, setWiwName] = useState('');
@@ -34,22 +35,28 @@ const Map = () => {
         })
         .then(res => {
             const places = res.data.map(place => place.addr['_text'])
-            setPlaces(places)
-            let positions = []
-            for (let i = 0; i < places.length; i++) {
-                fetch(`https://dapi.kakao.com/v2/local/search/address?query=${places[i]}`, {
+            const positions = []
+            places.forEach((place, i) => {
+                fetch(`https://dapi.kakao.com/v2/local/search/address?query=${place}`, {
                     headers: {
                         'Authorization': 'KakaoAK 804d239e2529038f08230bcfcf217433'
                     }
                 })
                 .then(res => res.json())
                 .then(json => {
-                    let coordinate = [Number(json.documents[0]['road_address']['x']), Number(json.documents[0]['road_address']['y'])]
+                    let coordinate = []
+                    coordinate.push(Number(json.documents[0].address.y))
+                    coordinate.push(Number(json.documents[0].address.x))
                     positions.push(coordinate)
+                    return positions
                 })
-            }
-            setMarkerPositions(positions)
-        });
+                .then(positions => {
+                    if (i === 9) {
+                        setMarkerPositions(positions)
+                    }  
+                })
+            })
+        })
     }
     return (
         <div id="map-page">
@@ -62,7 +69,7 @@ const Map = () => {
                 </select>
                 <input value={sdName} placeholder={'서울특별시'} onChange={handleSdName}></input>
                 <input value={wiwName} placeholder={'종로구'} onChange={handleWiwName}></input>
-                <button onClick={() => getPlaces()}>내 주변 투표소 찾기</button>
+                <button onClick={getPlaces}>내 주변 투표소 찾기</button>
             </div>
             <div id="map-box">
                 <KakaoMap markerPositions={markerPositions}/>
