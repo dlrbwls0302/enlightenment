de3ec1452a9aa9dfab6176e9c6abbeb49e21a9f8
juyeop <shininggiver@gmail.com>
2021-04-24 05:18:09 +0900

    [main 기능 대부분 구현][검색 및 매거진 좋아요 기능 미완성]

--- src/components/Magazine.js
+++ src/components/Magazine.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { AiFillHeart } from 'react-icons/ai';
 
-const Magazine = ({ index, id, like, title, value, description, createdAt, handleTogleMagazine }) => {
+const Magazine = ({ index, id, magazineUserId, like, title, value, description, createdAt, handleTogleMagazine }) => {
     const imgUrl = "https://s3.ap-northeast-2.amazonaws.com/www.kelection.ml/images/";
     const dummyImg = `https://s3.ap-northeast-2.amazonaws.com/www.kelection.ml/dummyImg${index <= 10 ? index : index % 10}.jpeg`;
     // http://localhost:5000/magazines/:magazineId/like
     return (
-        <div className="magazine" onClick={() => handleTogleMagazine(id, title, description, like, createdAt)} style={value === undefined ? { backgroundImage: `url(${dummyImg})` } :
+        <div className="magazine" onClick={() => handleTogleMagazine(id, magazineUserId, title, description, like, createdAt)} style={value === undefined ? { backgroundImage: `url(${dummyImg})` } :
             { backgroundImage: `url(${imgUrl}${value})` }}>
             <AiFillHeart className="magazine-heart" color="#d32f2f"></AiFillHeart>
             <p className="magazine-heart-count">{like}</p>
