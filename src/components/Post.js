c341157fdc19ca0c7bdb08d69471102c7089120f
sunghyun <constate@naver.com>
2021-04-23 17:42:46 +0900

    [Xfile_working][Xfile, Post]

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,29 +4,25 @@ import { Link } from 'react-router-dom';
 import { FcLike } from 'react-icons/fc';
 import { FcLikePlaceholder } from 'react-icons/fc';
 
-const Post = ({ title, like, description }) => {
+const Post = ({ title, like, description, createdAt }) => {
     const [toggle, setToggle] = useState(false);
+    createdAt = createdAt.slice(0, 10);
     return (
-        <div className='post-container'>
-            <div className='post-show'>
-                <h1 className='post-heading'>{title}</h1>
-                <div className='post-info'>
-                    <div className='post-info-one'>
-                        <div className='post-user-id'>codestates@gmail.com</div>
-                        <div className='post-user-id'>2021-04-20</div>
-                    </div>
-                    <div className='post-info-two'>
-                        <div className='post-user-id' onClick={() => { setToggle(!toggle) }}>
-                            {toggle ? <FcLike /> : <FcLikePlaceholder />}
-                        </div>
-                        <div className='post-user-id'>{like}</div>
-                        <div className='post-user-id'>수정</div>
-                        <div className='post-user-id'>삭제</div>
-                    </div>
+        <div className="post-container">
+            <div className="post-info-wrap">
+                <h1 className="post-title">{title}</h1>
+                <div className="post-infomation">
+                    <p className="post-likeBtn" onClick={() => { setToggle(!toggle) }}>
+                        {toggle ? <FcLike /> : <FcLikePlaceholder />}
+                    </p>
+                    <span className="post-like">{like}</span>
+                    <span className="post-modify">수정</span>
+                    <span className="post-delete">삭제</span>
+                    <div className="post-created-date">{createdAt}</div>
                 </div>
-                <div className='post-main-content' dangerouslySetInnerHTML={{ __html: description }}></div>
             </div>
-        </div>
+            <div className="post-main-content" dangerouslySetInnerHTML={{ __html: description }}></div>
+        </div >
     );
 };
 
